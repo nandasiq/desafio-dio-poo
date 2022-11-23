9e4ee656e4fe6fe192b9b4747803ff5a4a5260ea
Fernanda Siqueira <nandasiq@gmail.com>
2022-11-23 12:39:59 -0300

    criando as classes e seus atributos

---
+++ desafioPoo/src/desafioDioPoo/Main.java
@@ -0,0 +1,33 @@
+package desafioDioPoo;
+
+import java.time.LocalDate;
+
+import desafioDioPoo.dominio.Curso;
+import desafioDioPoo.dominio.Mentoria;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Curso curso1 = new Curso();
+		curso1.setTitulo("Curso Java");
+		curso1.setDescricao("Descricao do curso");
+		curso1.setCargaHoraria(8);
+	
+		Curso curso2 = new Curso();
+		curso2.setTitulo("Curso Python");
+		curso2.setDescricao("Descricao do curso Python");
+		curso2.setCargaHoraria(6);
+		
+		Mentoria mentoria = new Mentoria();
+		mentoria.setTitulo("Mentoria de Java");
+		mentoria.setDescricao("Descricao mentoria");
+		mentoria.setData(LocalDate.now());
+
+		System.out.println(curso1);
+		System.out.println(curso2);
+		System.out.println(mentoria);
+	
+	}
+	
+}
